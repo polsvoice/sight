7a892fb98fc6bbd9c3d797a0efc7a8c61b1d67a2
Brent Bartlett <brent.bartlett1@gmail.com>
2019-03-06 13:59:36 -0800

    added Room constructor

--- app/game.js
+++ app/game.js
@@ -1,1 +1,8 @@
-
+exports.returnGameMap = function(){
+    function Room(desc){
+        this.desc = desc;
+    }
+    let gameMap = [];
+    gameMap.push(new Room("A small, well-lit room."));
+    return gameMap;
+};
--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 var expect = require("chai").expect;
 var sight = require("../app/game.js");
 
-describe("Narration", function(){
-    it("describes the room when you enter it", function(){
-        let description = "A well-lit room.";
-        expect(sight.narrate(description)).to.equal(description);
+describe("Game Map", function(){
+    it("creates a game map", function(){
+        expect(sight.returnGameMap().length).to.equal(1);
+    });
+    it("describes the room", function(){
+        let arr = sight.returnGameMap();
+        expect(arr[0].desc).to.equal("A small, well-lit room.");
     });
 });
