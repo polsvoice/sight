fb709cd63d6b8f46d6903e1ccd9c703b20f6fd4d
Brent Bartlett <brent.bartlett1@gmail.com>
2019-03-06 14:25:11 -0800

    changed to explicit assignment

--- app/game.js
+++ app/game.js
@@ -3,7 +3,7 @@ exports.returnGameMap = function(){
         this.desc = desc;
     }
     let gameMap = [[],[],[]];
-    gameMap[0].push(new Room("A small, well-lit room."));
-    gameMap[0].push(new Room("A sparse kitchen. There is a toaster here."));
+    gameMap[0][0] = new Room("A small, well-lit room.");
+    gameMap[0][1]= new Room("A sparse kitchen. There is a toaster here.");
     return gameMap;
 };
